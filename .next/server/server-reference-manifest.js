3e77603e964b7cacadc88828a7793387538dab26
colly-m <sindanicolly@gmail.com>
2026-05-23 11:14:55 +0300

    chore: ignore build artifacts

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rXJU8poSSTxJnwd2SDFWESDv8jGjpjLmAzAO/5vkibU=\"\n}"
--- .next/static/chunks/app_page_tsx_80b15250._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_5d45ff73._.js",
-  "static/chunks/components_sections_2f075dad._.js"
-],
-    source: "dynamic"
-});
