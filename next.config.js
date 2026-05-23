686f444296fcc0400eca5bb8fe0c64f46440eefd
colly-m <sindanicolly@gmail.com>
2026-05-23 12:47:17 +0300

    Turbo remove update

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   images: {
     domains: ['media.licdn.com'],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': process.cwd(),
-      '@/components': `${process.cwd()}/components`,
-    }
-    return config
-  }
 }
 
 module.exports = nextConfig
